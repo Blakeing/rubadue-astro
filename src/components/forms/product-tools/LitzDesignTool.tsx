59c1c0ea654ce0a8765b71f79f488db7ffafd078
Blake Ingenthron <blakeingenthron@gmail.com>
2025-08-24 22:58:05 -0500

    refactor: remove unnecessary wrapper div from LitzDesignTool for cleaner layout

--- src/components/forms/product-tools/LitzDesignTool.tsx
+++ src/components/forms/product-tools/LitzDesignTool.tsx
@@ -355,7 +355,6 @@ export function LitzDesignToolV2() {
 
 	return (
 		<TooltipProvider>
-			<div className="max-w-7xl py-6 sm:py-12 md:py-16  mx-auto p-4 sm:p-6 space-y-6 sm:space-y-8">
 				<div className="grid grid-cols-1 xl:grid-cols-5 gap-6 sm:gap-8">
 					{/* Input Form */}
 					<Card className="xl:col-span-2">
@@ -1362,7 +1361,6 @@ export function LitzDesignToolV2() {
 						)}
 					</div>
 				</div>
-			</div>
 		</TooltipProvider>
 	);
 }
